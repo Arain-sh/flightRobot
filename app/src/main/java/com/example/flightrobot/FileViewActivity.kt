ba56e64ff2e40e0e83edc7fe486feb3d6db30564
Arain-sh <liu2596615@163.com>
2020-12-08 13:01:25 +0800

    add url display

---
+++ app/src/main/java/com/example/flightrobot/FileViewActivity.kt
@@ -0,0 +1,11 @@
+package com.example.flightrobot
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FileViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_file_view)
+    }
+}
